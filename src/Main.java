8709d1337f252939d051ef6c683504e7e6dcd03a
achin284 <achin284@student.fuhsd.org>
2026-04-14 12:41:28 -0700

    classes added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-	}
-
 }
---
+++ src/client/ClientMain.java
@@ -0,0 +1,11 @@
+package client;
+
+public class ClientMain {
+
+	ServerManager serverManager;
+	Window window;
+	GeographicalLocation location;
+	CartManager cartManager;
+	
+	
+}
